b980ea770292e751a9c8a8b9292d5244fb9c4425
Vermasaiyam <vermasaiyam9@gmail.com>
2024-10-16 01:08:26 +0530

    login page

--- frontend/src/auth/Login.tsx
+++ frontend/src/auth/Login.tsx
@@ -2,10 +2,14 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Separator } from "@/components/ui/separator"
 import { Loader2, LockKeyhole, Mail } from "lucide-react"
-import { useState } from "react"
+import { ChangeEvent, useState } from "react"
 import { Link } from "react-router-dom"
 import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
 
+type LoginInputState = {
+    email: string;
+    password: string;
+}
 
 const Login = () => {
     const [show, setShow] = useState(false);
@@ -13,10 +17,26 @@ const Login = () => {
 
     const loading = false;
 
+    const [input, setInput] = useState<LoginInputState>({
+        email: "",
+        password: "",
+    });
+
+    const changeEventHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setInput({ ...input, [name]: value });
+    }
+
+    const loginSubmitHandler = async (e: FormEvent) => {
+        e.preventDefault();
+        console.log(input);
+        
+    };
+
     return (
         <div className="flex items-center justify-center min-h-screen">
             <form
-                // onSubmit={loginSubmitHandler}
+                onSubmit={loginSubmitHandler}
                 className="md:p-8 w-full max-w-md rounded-lg md:border border-gray-200 mx-4 shadow-sm"
             >
                 <div className="mb-4">
@@ -28,8 +48,8 @@ const Login = () => {
                             type="email"
                             placeholder="Email"
                             name="email"
-                            // value={input.email}
-                            // onChange={changeEventHandler}
+                            value={input.email}
+                            onChange={changeEventHandler}
                             className="pl-10 focus-visible:ring-1"
                         />
                         <Mail className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
@@ -44,8 +64,8 @@ const Login = () => {
                             type={show ? "text" : "password"}
                             placeholder="Password"
                             name="password"
-                            // value={input.password}
-                            // onChange={changeEventHandler}
+                            value={input.password}
+                            onChange={changeEventHandler}
                             className="pl-10 focus-visible:ring-1"
                         />
                         <LockKeyhole className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
