fc0ad000f094f917b51dce524331e9158bd4dc54
Vermasaiyam <vermasaiyam9@gmail.com>
2024-10-26 22:04:18 +0530

    menu page

--- frontend/src/components/Menu.tsx
+++ frontend/src/components/Menu.tsx
@@ -1,5 +1,6 @@
+import { Heart } from "lucide-react";
 import { Button } from "./ui/button";
-import { Card, CardContent, CardFooter } from "./ui/card";
+import { Card, CardContent } from "./ui/card";
 
 const Menu = () => {
     return (
@@ -9,10 +10,13 @@ const Menu = () => {
             </h1>
             <div className="grid md:grid-cols-3 space-y-4 md:space-y-0">
                 {["biryani", "momos", "noodles"].map((menu: string) => (
-                    <Card className="max-w-xs mx-auto shadow-lg rounded-lg overflow-hidden">
-                        <img src="bg.png" alt="Menu Image" className="w-full h-40 object-cover" />
+                    <Card className="max-w-xs mx-auto shadow-lg rounded-lg overflow-hidden relative">
+                        <img src="https://img.freepik.com/free-photo/top-view-table-full-delicious-food-composition_23-2149141352.jpg" alt="Menu Image" className="w-full h-40 object-cover" />
+                        <div className="absolute top-2 left-2 bg-white rounded-full p-1 cursor-pointer">
+                            <Heart className="w-5 h-5" />
+                        </div>
                         <CardContent className="p-4">
-                            <h2 className="text-xl font-semibold text-gray-800 dark:text-white">
+                            <h2 className="text-xl font-bold text-gray-800 dark:text-white">
                                 {/* {menu.name} */}
                                 {menu}
                             </h2>
@@ -20,15 +24,25 @@ const Menu = () => {
                                 {/* {menu.description} */}
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex et perspiciatis cumque impedit similique atque.
                             </p>
-                            <h3 className="text-lg font-semibold mt-4">
-                                Price:
-                                <span className="text-green">
+                            <div className="flex flex-row justify-between items-center mt-4">
+                                <h3 className="text-lg font-bold text-green">
                                     {/* ₹{menu.price} */}
-                                    150
-                                </span>
-                            </h3>
+                                    ₹150
+                                </h3>
+
+                                <Button
+                                    variant={"outline"}
+                                    onClick={() => {
+                                        // addToCart(menu);
+                                        // navigate("/cart");
+                                    }}
+                                    className="rounded-full border border-green text-green hover:bg-green hover:text-white"
+                                >
+                                    Add to Cart
+                                </Button>
+                            </div>
                         </CardContent>
-                        <CardFooter className="p-4">
+                        {/* <CardFooter className="p-4">
                             <Button
                                 onClick={() => {
                                     // addToCart(menu);
@@ -38,7 +52,7 @@ const Menu = () => {
                             >
                                 Add to Cart
                             </Button>
-                        </CardFooter>
+                        </CardFooter> */}
                     </Card>
                 ))}
             </div>
