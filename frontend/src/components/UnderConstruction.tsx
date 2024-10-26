dd417eeb3a0b7e26a7e7274bb10f25f714586d9c
Vermasaiyam <vermasaiyam9@gmail.com>
2024-10-26 19:55:30 +0530

    navigate feature

--- frontend/src/components/UnderConstruction.tsx
+++ frontend/src/components/UnderConstruction.tsx
@@ -1,18 +1,22 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const UnderConstruction = () => {
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4 text-center">
-            <h1 className="text-3xl md:text-5xl font-bold mb-4">🚧 Under Construction 🚧</h1>
-            <p className="text-base md:text-lg text-gray-600 max-w-md">
-                This page is currently under construction. Please check back later!
-            </p>
-            <Link to={'/'} className="mt-6 bg-green hover:bg-hoverGreen text-white font-semibold py-2 px-4 rounded-md"
-            >
-                Go Back
-            </Link>
-        </div>
-    );
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4 text-center">
+      <h1 className="text-3xl md:text-5xl font-bold mb-4">🚧 Under Construction 🚧</h1>
+      <p className="text-base md:text-lg text-gray-600 max-w-md">
+        This page is currently under construction. Please check back later!
+      </p>
+      <button
+        onClick={() => navigate(-1)}
+        className="mt-6 bg-green hover:bg-hoverGreen text-white font-semibold py-2 px-4 rounded-md"
+      >
+        Go Back
+      </button>
+    </div>
+  );
 };
 
 export default UnderConstruction;
