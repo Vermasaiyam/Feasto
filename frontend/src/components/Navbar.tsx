0648f2b4d381ac436c13b64e0fc247fb62485840
Vermasaiyam <vermasaiyam9@gmail.com>
2024-10-26 12:54:50 +0530

    responsive navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+// import { useState } from "react";
 import { Link } from "react-router-dom";
 import {
   Menubar,
@@ -8,7 +8,7 @@ import {
   MenubarTrigger,
 } from "@/components/ui/menubar"
 import { Button } from "./ui/button";
-import { Loader2, Moon, ShoppingCart, Sun } from "lucide-react";
+import { HandPlatter, Loader2, Menu, Moon, PackageCheck, ShoppingCart, SquareMenu, Sun, User, UtensilsCrossed } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import {
   DropdownMenu,
@@ -17,18 +17,24 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 // import InitialsAvatar from 'react-initials-avatar';
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import { Separator } from "./ui/separator";
 
 
 
 const Navbar = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const admin: boolean = true;
   const loading: boolean = false;
   const cart = [];
-
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
 
   return (
     <nav className="bg-white px-4 shadow-sm">
@@ -44,8 +50,8 @@ const Navbar = () => {
           <Link to="/" className="hover:text-hoverGreen font-medium">Home</Link>
           <Link to="/profile" className="hover:text-hoverGreen  font-medium">Profile</Link>
           <Link to="/order/status" className="hover:text-hoverGreen  font-medium">Order</Link>
-          <Link to="/foods" className="hover:text-hoverGreen  font-medium">Foods</Link>
-          <Link to="/restaurants" className="hover:text-hoverGreen  font-medium">Restaurants</Link>
+          <Link to="/menu" className="hover:text-hoverGreen  font-medium">Menu</Link>
+          <Link to="/restaurants" className="hover:text-hoverGreen  font-medium">Restaurant</Link>
 
           {admin && (
             <Menubar>
@@ -121,113 +127,126 @@ const Navbar = () => {
           </div>
         </div>
 
-        {/* Mobile Menu Button */}
-        <div className="md:hidden">
-          <button
-            onClick={toggleMenu}
-            className="text-gray-700 hover:text-gray-900 focus:outline-none"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-6 h-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              {isMenuOpen ? (
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              ) : (
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              )}
-            </svg>
-          </button>
+        <div className="md:hidden lg:hidden">
+          <MobileNavbar />
         </div>
       </div>
 
-      {/* Mobile Menu */}
-      {isMenuOpen && (
-        <div className="flex w-[100%] justify-end">
-          <div
-            className={`fixed md:top-20 top-14 right-0 h-screen bg-white w-[40vw] md:hidden shadow-lg transition-transform duration-500 ease-in-out transform ${isMenuOpen ? 'translate-x-0' : 'translate-x-full'
-              }`}
-          >
-            <div className="flex flex-col gap-2 items-end px-4 space-y-2 mt-4">
-              <Link to="/" className="hover:text-hoverGreen font-medium">Home</Link>
-              <Link to="/profile" className="hover:text-hoverGreen  font-medium">Profile</Link>
-              <Link to="/order/status" className="hover:text-hoverGreen  font-medium">Order</Link>
-              <Link to="/foods" className="hover:text-hoverGreen  font-medium">Foods</Link>
-              <Link to="/restaurants" className="hover:text-hoverGreen  font-medium">Restaurants</Link>
-
-              {admin && (
-                <Menubar>
-                  <MenubarMenu>
-                    <MenubarTrigger className="cursor-pointer">Dashboard</MenubarTrigger>
-                    <MenubarContent>
-                      <Link to="/admin/restaurant">
-                        <MenubarItem className="cursor-pointer">Restaurant</MenubarItem>
-                      </Link>
-                      <Link to="/admin/menu">
-                        <MenubarItem className="cursor-pointer">Menu</MenubarItem>
-                      </Link>
-                      <Link to="/admin/orders">
-                        <MenubarItem className="cursor-pointer">Orders</MenubarItem>
-                      </Link>
-                    </MenubarContent>
-                  </MenubarMenu>
-                </Menubar>
-              )}
-            </div>
-
-            <div className="flex space-x-4 px-4 mt-4 justify-end w-full">
-              <button className="bg-gray-300 hover:bg-gray-400 text-white font-bold py-2 px-4 rounded-full">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M12 4.5v15m7.5-7.5h-15"
-                  />
-                </svg>
-              </button>
-              <button className="bg-gray-300 hover:bg-gray-400 text-white font-bold py-2 px-4 rounded-full">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M12 4.5v15m7.5-7.5h-15"
-                  />
-                </svg>
-              </button>
-            </div>
-          </div>
-        </div>
-
-      )}
     </nav>
   )
 }
 
 export default Navbar
+
+const MobileNavbar = () => {
+  // const { user, logout, loading } = useUserStore();
+  // const { setTheme } = useThemeStore();
+
+  const admin: boolean = true;
+  const loading: boolean = false;
+
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button
+          size={"icon"}
+          className="rounded-full bg-gray-200 text-black hover:bg-gray-200"
+          variant="outline"
+        >
+          <Menu size={"18"} />
+        </Button>
+      </SheetTrigger>
+      <SheetContent className="flex flex-col">
+        <SheetHeader className="flex flex-row items-center justify-between mt-2">
+          <SheetTitle className="text-hoverGreen">FEASTO</SheetTitle>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" size="icon">
+                <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                <span className="sr-only">Toggle theme</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem>Light</DropdownMenuItem>
+              <DropdownMenuItem >Dark</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </SheetHeader>
+        <Separator className="my-2" />
+        <SheetDescription className="flex-1">
+          <Link
+            to="/profile"
+            className="flex items-center gap-4 hover:bg-gray-200 px-3 py-2 rounded-lg cursor-pointer hover:text-gray-900 font-medium"
+          >
+            <User />
+            <span>Profile</span>
+          </Link>
+          <Link
+            to="/order/status"
+            className="flex items-center gap-4 hover:bg-gray-200 px-3 py-2 rounded-lg cursor-pointer hover:text-gray-900 font-medium"
+          >
+            <HandPlatter />
+            <span>Order</span>
+          </Link>
+          <Link
+            to="/cart"
+            className="flex items-center gap-4 hover:bg-gray-200 px-3 py-2 rounded-lg cursor-pointer hover:text-gray-900 font-medium"
+          >
+            <ShoppingCart />
+            <span>Cart (0)</span>
+          </Link>
+          {admin && (
+            <>
+              <Link
+                to="/admin/menu"
+                className="flex items-center gap-4 hover:bg-gray-200 px-3 py-2 rounded-lg cursor-pointer hover:text-gray-900 font-medium"
+              >
+                <SquareMenu />
+                <span>Menu</span>
+              </Link>
+              <Link
+                to="/admin/restaurant"
+                className="flex items-center gap-4 hover:bg-gray-200 px-3 py-2 rounded-lg cursor-pointer hover:text-gray-900 font-medium"
+              >
+                <UtensilsCrossed />
+                <span>Restaurant</span>
+              </Link>
+              <Link
+                to="/admin/orders"
+                className="flex items-center gap-4 hover:bg-gray-200 px-3 py-2 rounded-lg cursor-pointer hover:text-gray-900 font-medium"
+              >
+                <PackageCheck />
+                <span>Restaurant Orders</span>
+              </Link>
+            </>
+          )}
+        </SheetDescription>
+        <SheetFooter className="flex flex-col gap-4">
+          <div className="flex flex-row items-center gap-2">
+            <Avatar>
+              <AvatarImage src="" />
+              <AvatarFallback>SV</AvatarFallback>
+            </Avatar>
+            <h1 className="font-bold">Saiyam Verma</h1>
+          </div>
+          <SheetClose asChild>
+            {loading ? (
+              <Button className="bg-green hover:bg-hoverGreen">
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                Please wait
+              </Button>
+            ) : (
+              <Button
+                // onClick={logout}
+                className="bg-green hover:bg-hoverGreen"
+              >
+                Logout
+              </Button>
+            )}
+          </SheetClose>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
+  );
+};
