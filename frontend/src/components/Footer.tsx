b14f2cb487cd97515fe6582d21b8b286477ad73c
Vermasaiyam <vermasaiyam9@gmail.com>
2024-10-26 22:54:27 +0530

    title change in footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -14,19 +14,19 @@ const Footer = () => {
                         Satisfy Your Cravings with a Click!
                     </p>
                     <div className="flex flex-row gap-2 w-full items-center justify-center">
-                        <Link to={"https://github.com/Vermasaiyam"} target="blank" className="bg-white p-1.5 rounded-full flex items-center justify-center">
+                        <Link to={"https://github.com/Vermasaiyam"} target="blank" title="Github" className="bg-white p-1.5 rounded-full flex items-center justify-center">
                             <Github className="w-5 h-5" />
                         </Link>
-                        <Link to={"https://www.linkedin.com/in/saiyam05/"} target="blank" className="bg-white p-1.5 rounded-full flex items-center justify-center">
+                        <Link to={"https://www.linkedin.com/in/saiyam05/"} target="blank" title="LinkedIn" className="bg-white p-1.5 rounded-full flex items-center justify-center">
                             <Linkedin className="w-5 h-5" />
                         </Link>
-                        <Link to={"https://x.com/SaiyamVerm91813/"} target="blank" className="bg-white p-1.5 rounded-full flex items-center justify-center">
+                        <Link to={"https://x.com/SaiyamVerm91813/"} target="blank" title="X" className="bg-white p-1.5 rounded-full flex items-center justify-center">
                             <X className="w-5 h-5" />
                         </Link>
-                        <Link to={"https://www.instagram.com/s.verma0504/"} target="blank" className="bg-white p-1.5 rounded-full flex items-center justify-center">
+                        <Link to={"https://www.instagram.com/s.verma0504/"} target="blank" title="Instagram" className="bg-white p-1.5 rounded-full flex items-center justify-center">
                             <Instagram className="w-5 h-5" />
                         </Link>
-                        <Link to={"mailto:vermasaiyam9@gmail.com"} target="blank" className="bg-white p-1.5 rounded-full flex items-center justify-center">
+                        <Link to={"mailto:vermasaiyam9@gmail.com"} target="blank" title="E-mail" className="bg-white p-1.5 rounded-full flex items-center justify-center">
                             <Mail className="w-5 h-5" />
                         </Link>
                     </div>
