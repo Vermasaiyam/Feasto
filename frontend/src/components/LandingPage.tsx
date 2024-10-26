96a4dd882eb3d1ebf4089c11f1a5a3a2d08f0e56
Vermasaiyam <vermasaiyam9@gmail.com>
2024-10-26 21:39:54 +0530

    search on Enter

--- frontend/src/components/LandingPage.tsx
+++ frontend/src/components/LandingPage.tsx
@@ -9,6 +9,12 @@ const LandingPage = () => {
     const [searchText, setSearchText] = useState<string>("");
     const navigate = useNavigate();
 
+    const keyDown = (e: React.KeyboardEvent<HTMLInputElement>)=>{
+        if(e.key === 'Enter'){
+            navigate(`/search/${searchText}`);
+        }
+    }
+
     return (
         <div className="">
             <div className="flex flex-col md:flex-row max-w-8xl lg:mx-20 md:mx-12 mx-4 py-4 px-6 bg-lightGreen md:p-10 rounded-lg items-center justify-around m-4 gap-20 box-border">
@@ -28,6 +34,7 @@ const LandingPage = () => {
                             placeholder="Search restaurant by name, city or country."
                             onChange={(e) => setSearchText(e.target.value)}
                             className="pl-10 shadow-lg"
+                            onKeyDown={(e) => keyDown(e)}
                         />
                         <Search className="text-gray-500 absolute inset-y-2 left-2" />
                         <Button onClick={() => navigate(`/search/${searchText}`)} className="px-2 md:py-1.5 py-2 md:text-base text-sm text-white shadow-lg bg-green hover:bg-hoverGreen rounded-sm">Search</Button>
