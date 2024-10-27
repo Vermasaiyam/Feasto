8facdc5d13c2fe10e21a3aaad73f7b6e5894db2c
Vermasaiyam <vermasaiyam9@gmail.com>
2024-10-27 14:35:59 +0530

    add menu page

--- frontend/src/admin/AddMenu.tsx
+++ frontend/src/admin/AddMenu.tsx
@@ -15,32 +15,44 @@ import { useState } from "react";
 
 
 const AddMenu = () => {
-    const [input, setInput] = useState<MenuFormSchema>({
+    const [input, setInput] = useState({
         name: "",
         description: "",
         price: 0,
         image: undefined,
     });
     const [open, setOpen] = useState<boolean>(false);
+    const [editOpen, setEditOpen] = useState<boolean>(false);
+    const [selectedMenu, setSelectedMenu] = useState<any>();
+    const loading: boolean = false;
+
+    const changeEventHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value, type } = e.target;
+        setInput({ ...input, [name]: type === "number" ? Number(value) : value });
+    };
+
+    const submitHandler = () => {
+
+    }
 
     return (
         <div className="max-w-6xl mx-auto my-10">
             <div className="flex justify-between">
                 <h1 className="font-bold md:font-extrabold text-lg md:text-2xl">
-                    Available Menus
+                    My Menu
                 </h1>
                 <Dialog open={open} onOpenChange={setOpen}>
                     <DialogTrigger>
-                        <Button className="bg-orange hover:bg-hoverOrange">
+                        <Button className="bg-green hover:bg-hoverGreen">
                             <Plus className="mr-2" />
-                            Add Menus
+                            Add Item
                         </Button>
                     </DialogTrigger>
                     <DialogContent>
                         <DialogHeader>
-                            <DialogTitle>Add A New Menu</DialogTitle>
+                            <DialogTitle>Add A New Item</DialogTitle>
                             <DialogDescription>
-                                Create a menu that will make your restaurant stand out.
+                                Design a menu that will make your restaurant unforgettable.
                             </DialogDescription>
                         </DialogHeader>
                         <form onSubmit={submitHandler} className="space-y-4">
@@ -51,13 +63,13 @@ const AddMenu = () => {
                                     name="name"
                                     value={input.name}
                                     onChange={changeEventHandler}
-                                    placeholder="Enter menu name"
+                                    placeholder="Enter item name"
                                 />
-                                {error && (
+                                {/* {error && (
                                     <span className="text-xs font-medium text-red-600">
                                         {error.name}
                                     </span>
-                                )}
+                                )} */}
                             </div>
                             <div>
                                 <Label>Description</Label>
@@ -66,55 +78,55 @@ const AddMenu = () => {
                                     name="description"
                                     value={input.description}
                                     onChange={changeEventHandler}
-                                    placeholder="Enter menu description"
+                                    placeholder="Enter item description"
                                 />
-                                {error && (
+                                {/* {error && (
                                     <span className="text-xs font-medium text-red-600">
                                         {error.description}
                                     </span>
-                                )}
+                                )} */}
                             </div>
                             <div>
-                                <Label>Price in (Rupees)</Label>
+                                <Label>Price</Label>
                                 <Input
                                     type="number"
                                     name="price"
                                     value={input.price}
                                     onChange={changeEventHandler}
-                                    placeholder="Enter menu price"
+                                    placeholder="Enter item price"
                                 />
-                                {error && (
+                                {/* {error && (
                                     <span className="text-xs font-medium text-red-600">
                                         {error.price}
                                     </span>
-                                )}
+                                )} */}
                             </div>
                             <div>
                                 <Label>Upload Menu Image</Label>
                                 <Input
                                     type="file"
                                     name="image"
-                                    onChange={(e) =>
-                                        setInput({
-                                            ...input,
-                                            image: e.target.files?.[0] || undefined,
-                                        })
-                                    }
+                                // onChange={(e) =>
+                                //     setInput({
+                                //         ...input,
+                                //         image: e.target.files?.[0] || undefined,
+                                //     })
+                                // }
                                 />
-                                {error && (
+                                {/* {error && (
                                     <span className="text-xs font-medium text-red-600">
                                         {error.image?.name}
                                     </span>
-                                )}
+                                )} */}
                             </div>
                             <DialogFooter className="mt-5">
                                 {loading ? (
-                                    <Button disabled className="bg-orange hover:bg-hoverOrange">
+                                    <Button disabled className="bg-green hover:bg-hoverGreen">
                                         <Loader2 className="mr-2 w-4 h-4 animate-spin" />
                                         Please wait
                                     </Button>
                                 ) : (
-                                    <Button className="bg-orange hover:bg-hoverOrange">
+                                    <Button className="bg-green hover:bg-hoverGreen">
                                         Submit
                                     </Button>
                                 )}
@@ -123,21 +135,21 @@ const AddMenu = () => {
                     </DialogContent>
                 </Dialog>
             </div>
-            {restaurant?.menus.map((menu: any, idx: number) => (
-                <div key={idx} className="mt-6 space-y-4">
+            {["momos", 'biryani', "paneer"].map((menu: string, idx: number) => (
+                <div key={idx} className="mt-6 space-y-4 hover:shadow-sm">
                     <div className="flex flex-col md:flex-row md:items-center md:space-x-4 md:p-4 p-2 shadow-md rounded-lg border">
                         <img
-                            src={menu.image}
-                            alt=""
+                            src=""
+                            alt="Item Image"
                             className="md:h-24 md:w-24 h-16 w-full object-cover rounded-lg"
                         />
                         <div className="flex-1">
                             <h1 className="text-lg font-semibold text-gray-800">
-                                {menu.name}
+                                {menu}
                             </h1>
-                            <p className="text-sm tex-gray-600 mt-1">{menu.description}</p>
+                            <p className="text-sm tex-gray-600 mt-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, culpa!</p>
                             <h2 className="text-md font-semibold mt-2">
-                                Price: <span className="text-[#D19254]">80</span>
+                                Price: <span className="text-green">80</span>
                             </h2>
                         </div>
                         <Button
@@ -146,18 +158,18 @@ const AddMenu = () => {
                                 setEditOpen(true);
                             }}
                             size={"sm"}
-                            className="bg-orange hover:bg-hoverOrange mt-2"
+                            className="bg-green hover:bg-hoverGreen mt-2"
                         >
                             Edit
                         </Button>
                     </div>
                 </div>
             ))}
-            <EditMenu
+            {/* <EditMenu
                 selectedMenu={selectedMenu}
                 editOpen={editOpen}
                 setEditOpen={setEditOpen}
-            />
+            /> */}
         </div>
     )
 }
