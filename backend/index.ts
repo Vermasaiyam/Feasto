785fe9e85e6d052ac3d8a22fbc33b197a77397dd
Vermasaiyam <vermasaiyam9@gmail.com>
2024-10-27 22:10:20 +0530

    mongodb connected

--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,23 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+// import connectDB from './db/ConnectDB';
+dotenv.config();
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI!);
+        console.log('mongoDB connected successfully.');
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log(`Server started at port ${PORT}`);
+});
