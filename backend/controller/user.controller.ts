4f6e09d51b7b8f2cf81e5135e44b1bb1d87146a4
Vermasaiyam <vermasaiyam9@gmail.com>
2024-10-28 12:53:16 +0530

    updateProfile

--- backend/controller/user.controller.ts
+++ backend/controller/user.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { User } from "../models/user.model";
 import bcrypt from "bcryptjs";
 import crypto from "crypto";
-// import cloudinary from "../utils/cloudinary";
+import cloudinary from "../utils/cloudinary";
 // import { generateVerificationCode } from "../utils/generateVerificationCode";
 // import { generateToken } from "../utils/generateToken";
 // import { sendPasswordResetEmail, sendResetSuccessEmail, sendVerificationEmail, sendWelcomeEmail } from "../mailtrap/email";
@@ -209,3 +209,25 @@ export const checkAuth = async (req: Request, res: Response) => {
         return res.status(500).json({ message: "Internal server error" });
     }
 };
+
+export const updateProfile = async (req: Request, res: Response) => {
+    try {
+        const userId = req.id;
+        const { fullname, email, address, city, country, profilePicture } = req.body;
+        // upload image on cloudinary
+        let cloudResponse: any;
+        cloudResponse = await cloudinary.uploader.upload(profilePicture);
+        const updatedData = { fullname, email, address, city, country, profilePicture };
+
+        const user = await User.findByIdAndUpdate(userId, updatedData, { new: true }).select("-password");
+
+        return res.status(200).json({
+            success: true,
+            user,
+            message: "Profile Updated Successfully"
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Internal Server Error" });
+    }
+}
