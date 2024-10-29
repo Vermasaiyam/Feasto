12cf7701fb709c0a9e1d54e7166415659f35c919
Vermasaiyam <vermasaiyam9@gmail.com>
2024-10-29 21:55:35 +0530

    integrating

--- frontend/src/admin/Restaurant.tsx
+++ frontend/src/admin/Restaurant.tsx
@@ -2,8 +2,9 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { restaurantFormSchema, RestaurantFormSchema } from "@/schema/restaurantSchema";
+import { useRestaurantStore } from "@/store/useRestaurantStore";
 import { Loader2 } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Restaurant = () => {
     const [input, setInput] = useState<RestaurantFormSchema>({
@@ -18,15 +19,23 @@ const Restaurant = () => {
     // partial isliye hai kyuki ho skta h sirf ek adh m error ho
     const [errors, setErrors] = useState<Partial<RestaurantFormSchema>>({});
 
-    const loading: boolean = false;
-    const restaurant: boolean = false;
+    const {
+        loading,
+        restaurant,
+        updateRestaurant,
+        createRestaurant,
+        getRestaurant,
+    } = useRestaurantStore();
+
+    // const loading: boolean = false;
+    // const restaurant: boolean = false;
 
     const changeEventHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value, type } = e.target;
         setInput({ ...input, [name]: type === "number" ? Number(value) : value });
     };
 
-    const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    const submitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
         const result = restaurantFormSchema.safeParse(input);
@@ -36,12 +45,58 @@ const Restaurant = () => {
             return;
         }
 
+        try {
+            const formData = new FormData();
+            formData.append("restaurantName", input.restaurantName);
+            formData.append("city", input.city);
+            formData.append("country", input.country);
+            formData.append("deliveryTime", input.deliveryTime.toString());
+            formData.append("cuisines", JSON.stringify(input.cuisines));
+
+            if (input.imageFile) {
+                formData.append("imageFile", input.imageFile);
+            }
+
+            if (restaurant) {
+                // update
+                await updateRestaurant(formData);
+            } else {
+                // create
+                await createRestaurant(formData);
+            }
+        } catch (error) {
+            console.log(error);
+        }
     }
+
+    useEffect(() => {
+        const fetchRestaurant = async () => {
+            await getRestaurant();
+            if (restaurant) {
+                setInput({
+                    restaurantName: restaurant.restaurantName || "",
+                    city: restaurant.city || "",
+                    country: restaurant.country || "",
+                    deliveryTime: restaurant.deliveryTime || 0,
+                    cuisines: restaurant.cuisines
+                        ? restaurant.cuisines.map((cuisine: string) => cuisine)
+                        : [],
+                    imageFile: undefined,
+                });
+            };
+        }
+        fetchRestaurant();
+        // console.log(restaurant);
+
+    }, []);
+
+
+
     return (
         <div className="max-w-6xl mx-auto my-10">
             <div className="mx-2">
                 <div>
-                    <h1 className="font-extrabold text-2xl mb-5">
+                    <h1 className="font-extrabold text-center text-2xl mb-5">
                         {
                             restaurant
                                 ? "Update Your Restaurant"
