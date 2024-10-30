cd0065f8483bb2cc720d6a2873ad235530a83770
Vermasaiyam <vermasaiyam9@gmail.com>
2024-10-30 14:01:10 +0530

    cart page

--- frontend/src/components/Cart.tsx
+++ frontend/src/components/Cart.tsx
@@ -12,97 +12,119 @@ import {
 } from "./ui/table";
 import { useState } from "react";
 import CheckoutPage from "./CheckoutPage";
+import { useCartStore } from "@/store/useCartStore";
+import { CartItem } from "@/types/cartType";
 
 const Cart = () => {
     const [open, setOpen] = useState<boolean>(false);
+    const { cart, decrementQuantity, incrementQuantity, removeFromTheCart } = useCartStore();
+
+    let totalAmount = cart.reduce((acc, ele) => {
+        return acc + ele.price * ele.quantity;
+    }, 0);
 
     return (
         <div className="flex flex-col max-w-7xl mx-auto my-10">
-            <div className="flex justify-end">
-                <Button variant="link">Clear All</Button>
-            </div>
-            <Table>
-                <TableHeader>
-                    <TableRow>
-                        <TableHead className="text-darkGreen text-lg">Items</TableHead>
-                        <TableHead className="text-darkGreen text-lg">Title</TableHead>
-                        <TableHead className="text-darkGreen text-lg">Price</TableHead>
-                        <TableHead className="text-darkGreen text-lg">Quantity</TableHead>
-                        <TableHead className="text-darkGreen text-lg">Total</TableHead>
-                        <TableHead className="text-right text-darkGreen text-lg">Remove</TableHead>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    {["biryani", "momos", "noodles"].map((item: string) => (
-                        <TableRow>
-                            <TableCell>
-                                <Avatar>
-                                    <AvatarImage src="" alt="" />
-                                    <AvatarFallback>SV</AvatarFallback>
-                                </Avatar>
-                            </TableCell>
-                            <TableCell> {item}</TableCell>
-                            <TableCell> 500</TableCell>
-                            <TableCell>
-                                <div className="flex items-center rounded-full">
-                                    <Button
-                                        // onClick={() => decrementQuantity(item._id)}
-                                        size={"icon"}
-                                        variant={"outline"}
-                                        className="rounded-full bg-green hover:bg-hoverGreen text-white hover:text-white"
-                                    >
-                                        <Minus className="w-4 h-4" />
-                                    </Button>
-                                    <Button
-                                        // size={"icon"}
-                                        className="font-bold border-none"
-                                        disabled
-                                        variant={"outline"}
-                                    >
-                                        {/* {item.quantity} */}
-                                        2
-                                    </Button>
-                                    <Button
-                                        // onClick={() => incrementQuantity(item._id)}
-                                        size={"icon"}
-                                        className="rounded-full bg-green hover:bg-hoverGreen text-white hover:text-white"
-                                        variant={"outline"}
-                                    >
-                                        <Plus className="w-4 h-4" />
-                                    </Button>
-                                </div>
-                            </TableCell>
-                            <TableCell>
-                                {/* {item.price * item.quantity} */}
-                                {500 * 2}
-                            </TableCell>
-                            <TableCell className="text-right">
-                                <Button size={"sm"} variant={"outline"} className="hover:text-white text-green border border-green hover:bg-green">
-                                    Remove
-                                </Button>
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-                <TableFooter>
-                    <TableRow className="text-2xl font-bold">
-                        <TableCell colSpan={5}>Total</TableCell>
-                        <TableCell className="text-right">
-                            {/* ₹{totalAmount} */}
-                            ₹5000
-                        </TableCell>
-                    </TableRow>
-                </TableFooter>
-            </Table>
-            <div className="flex justify-end my-5">
-                <Button
-                    onClick={() => setOpen(true)}
-                    className="bg-green hover:bg-hoverGreen"
-                >
-                    Proceed To Checkout
-                </Button>
-            </div>
-            <CheckoutPage open={open} setOpen={setOpen} />
+            {
+                cart.length === 0 ? (
+                    <div className="flex flex-col items-center justify-center gap-4 my-28">
+                        <span className="text-gray-600 md:text-base text-sm">No items in the cart.</span>
+                        <Button className="bg-green hover:bg-hoverGreen">
+                            Order Now
+                        </Button>
+                    </div>
+                ) : (
+                    <div className="">
+                        <div className="flex justify-end">
+                            <Button variant="link">Clear All</Button>
+                        </div>
+                        <Table>
+                            <TableHeader>
+                                <TableRow>
+                                    <TableHead className="text-darkGreen text-lg">Items</TableHead>
+                                    <TableHead className="text-darkGreen text-lg">Title</TableHead>
+                                    <TableHead className="text-darkGreen text-lg">Price</TableHead>
+                                    <TableHead className="text-darkGreen text-lg">Quantity</TableHead>
+                                    <TableHead className="text-darkGreen text-lg">Total</TableHead>
+                                    <TableHead className="text-right text-darkGreen text-lg">Remove</TableHead>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody>
+                                {cart.map((item: CartItem) => (
+                                    <TableRow>
+                                        <TableCell>
+                                            <Avatar>
+                                                <AvatarImage src={item.image} alt={item.name} />
+                                                <AvatarFallback>Item</AvatarFallback>
+                                            </Avatar>
+                                        </TableCell>
+                                        <TableCell> {item.name}</TableCell>
+                                        <TableCell> {item.price}</TableCell>
+                                        <TableCell>
+                                            <div className="flex items-center rounded-full">
+                                                <Button
+                                                    onClick={() => decrementQuantity(item._id)}
+                                                    size={"icon"}
+                                                    variant={"outline"}
+                                                    className="rounded-full bg-green hover:bg-hoverGreen text-white hover:text-white"
+                                                >
+                                                    <Minus className="w-4 h-4" />
+                                                </Button>
+                                                <Button
+                                                    // size={"icon"}
+                                                    className="font-bold border-none"
+                                                    disabled
+                                                    variant={"outline"}
+                                                >
+                                                    {item.quantity}
+                                                    {/* 2 */}
+                                                </Button>
+                                                <Button
+                                                    onClick={() => incrementQuantity(item._id)}
+                                                    size={"icon"}
+                                                    className="rounded-full bg-green hover:bg-hoverGreen text-white hover:text-white"
+                                                    variant={"outline"}
+                                                >
+                                                    <Plus className="w-4 h-4" />
+                                                </Button>
+                                            </div>
+                                        </TableCell>
+                                        <TableCell>
+                                            {item.price * item.quantity}
+                                            {/* {500 * 2} */}
+                                        </TableCell>
+                                        <TableCell className="text-right">
+                                            <Button onClick={() => removeFromTheCart(item._id)} size={"sm"} variant={"outline"} className="hover:text-white text-green border border-green hover:bg-green">
+                                                Remove
+                                            </Button>
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                            <TableFooter>
+                                <TableRow className="text-2xl font-bold">
+                                    <TableCell colSpan={5}>Total</TableCell>
+                                    <TableCell className="text-right">
+                                        ₹{totalAmount}
+                                        {/* ₹5000 */}
+                                    </TableCell>
+                                </TableRow>
+                            </TableFooter>
+                        </Table>
+                        <div className="flex justify-end my-5">
+                            <Button
+                                onClick={() => setOpen(true)}
+                                className="bg-green hover:bg-hoverGreen"
+                                disabled={cart.length === 0}
+                            >
+                                Proceed To Checkout
+                            </Button>
+                        </div>
+                        <CheckoutPage open={open} setOpen={setOpen} />
+
+                    </div>
+                )
+            }
         </div>
     )
 }
