a369a5ef8fad9417548afd0cc4ee3e3ce6b7f276
Vermasaiyam <vermasaiyam9@gmail.com>
2024-11-16 19:15:41 +0530

    changes

--- frontend/src/admin/AddMenu.tsx
+++ frontend/src/admin/AddMenu.tsx
@@ -31,7 +31,7 @@ const AddMenu = () => {
     // const loading: boolean = false;
 
 
-    const { loading, createMenu , deleteMenu} = useMenuStore();
+    const { loading, createMenu, deleteMenu } = useMenuStore();
     const { restaurant } = useRestaurantStore();
 
     const changeEventHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -98,99 +98,105 @@ const AddMenu = () => {
                 <h1 className="font-bold md:font-extrabold text-lg md:text-2xl">
                     My Menu
                 </h1>
-                <Dialog open={open} onOpenChange={setOpen}>
-                    <DialogTrigger>
-                        <Button className="bg-green hover:bg-hoverGreen dark:text-white">
-                            <Plus className="mr-2" />
-                            Add Item
-                        </Button>
-                    </DialogTrigger>
-                    <DialogContent>
-                        <DialogHeader>
-                            <DialogTitle>Add A New Item</DialogTitle>
-                            <DialogDescription>
-                                Design a menu that will make your restaurant unforgettable.
-                            </DialogDescription>
-                        </DialogHeader>
-                        <form onSubmit={submitHandler} className="space-y-4">
-                            <div>
-                                <Label>Name</Label>
-                                <Input
-                                    type="text"
-                                    name="name"
-                                    value={input.name}
-                                    onChange={changeEventHandler}
-                                    placeholder="Enter item name"
-                                />
-                                {error && (
-                                    <span className="text-xs font-medium text-red-600">
-                                        {error.name}
-                                    </span>
-                                )}
-                            </div>
-                            <div>
-                                <Label>Description</Label>
-                                <Input
-                                    type="text"
-                                    name="description"
-                                    value={input.description}
-                                    onChange={changeEventHandler}
-                                    placeholder="Enter item description"
-                                />
-                                {error && (
-                                    <span className="text-xs font-medium text-red-600">
-                                        {error.description}
-                                    </span>
-                                )}
-                            </div>
-                            <div>
-                                <Label>Price</Label>
-                                <Input
-                                    type="number"
-                                    name="price"
-                                    value={input.price}
-                                    onChange={changeEventHandler}
-                                    placeholder="Enter item price"
-                                />
-                                {error && (
-                                    <span className="text-xs font-medium text-red-600">
-                                        {error.price}
-                                    </span>
-                                )}
-                            </div>
-                            <div>
-                                <Label>Upload Menu Image</Label>
-                                <Input
-                                    type="file"
-                                    name="image"
-                                    onChange={(e) =>
-                                        setInput({
-                                            ...input,
-                                            image: e.target.files?.[0] || undefined,
-                                        })
-                                    }
-                                />
-                                {error && (
-                                    <span className="text-xs font-medium text-red-600">
-                                        {error.image?.name}
-                                    </span>
-                                )}
-                            </div>
-                            <DialogFooter className="mt-5">
-                                {loading ? (
-                                    <Button disabled className="w-full bg-green hover:bg-hoverGreen dark:text-white">
-                                        <Loader2 className="mr-2 w-4 h-4 animate-spin" />
-                                        Please wait
+                <div className="">
+                    {
+                        restaurant && (
+                            <Dialog open={open} onOpenChange={setOpen}>
+                                <DialogTrigger>
+                                    <Button className="bg-green hover:bg-hoverGreen dark:text-white">
+                                        <Plus className="mr-2" />
+                                        Add Item
                                     </Button>
-                                ) : (
-                                    <Button className="w-full bg-green hover:bg-hoverGreen dark:text-white">
-                                        Submit
-                                    </Button>
-                                )}
-                            </DialogFooter>
-                        </form>
-                    </DialogContent>
-                </Dialog>
+                                </DialogTrigger>
+                                <DialogContent>
+                                    <DialogHeader>
+                                        <DialogTitle>Add A New Item</DialogTitle>
+                                        <DialogDescription>
+                                            Design a menu that will make your restaurant unforgettable.
+                                        </DialogDescription>
+                                    </DialogHeader>
+                                    <form onSubmit={submitHandler} className="space-y-4">
+                                        <div>
+                                            <Label>Name</Label>
+                                            <Input
+                                                type="text"
+                                                name="name"
+                                                value={input.name}
+                                                onChange={changeEventHandler}
+                                                placeholder="Enter item name"
+                                            />
+                                            {error && (
+                                                <span className="text-xs font-medium text-red-600">
+                                                    {error.name}
+                                                </span>
+                                            )}
+                                        </div>
+                                        <div>
+                                            <Label>Description</Label>
+                                            <Input
+                                                type="text"
+                                                name="description"
+                                                value={input.description}
+                                                onChange={changeEventHandler}
+                                                placeholder="Enter item description"
+                                            />
+                                            {error && (
+                                                <span className="text-xs font-medium text-red-600">
+                                                    {error.description}
+                                                </span>
+                                            )}
+                                        </div>
+                                        <div>
+                                            <Label>Price</Label>
+                                            <Input
+                                                type="number"
+                                                name="price"
+                                                value={input.price}
+                                                onChange={changeEventHandler}
+                                                placeholder="Enter item price"
+                                            />
+                                            {error && (
+                                                <span className="text-xs font-medium text-red-600">
+                                                    {error.price}
+                                                </span>
+                                            )}
+                                        </div>
+                                        <div>
+                                            <Label>Upload Menu Image</Label>
+                                            <Input
+                                                type="file"
+                                                name="image"
+                                                onChange={(e) =>
+                                                    setInput({
+                                                        ...input,
+                                                        image: e.target.files?.[0] || undefined,
+                                                    })
+                                                }
+                                            />
+                                            {error && (
+                                                <span className="text-xs font-medium text-red-600">
+                                                    {error.image?.name}
+                                                </span>
+                                            )}
+                                        </div>
+                                        <DialogFooter className="mt-5">
+                                            {loading ? (
+                                                <Button disabled className="w-full bg-green hover:bg-hoverGreen dark:text-white">
+                                                    <Loader2 className="mr-2 w-4 h-4 animate-spin" />
+                                                    Please wait
+                                                </Button>
+                                            ) : (
+                                                <Button className="w-full bg-green hover:bg-hoverGreen dark:text-white">
+                                                    Submit
+                                                </Button>
+                                            )}
+                                        </DialogFooter>
+                                    </form>
+                                </DialogContent>
+                            </Dialog>
+                        )
+                    }
+                </div>
 
             </div>
             {restaurant?.menus.map((menu: any, idx: number) => (
